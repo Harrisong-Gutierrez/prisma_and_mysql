77351fb4c4299d7b226169f8fd70524349e0ad20
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-05-09 16:20:12 -0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,364 @@
-import Image from "next/image";
+"use client";
+
+import { User } from "@prisma/client";
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User>({
+    Id: 0,
+    Login: "",
+    FirstName: "",
+    LastName: "",
+    FullName: "",
+    DisplayName: "",
+    Email: "",
+    Password: "",
+    RegDate: new Date(),
+    UpdatedDate: new Date(),
+  });
+
+  const GetUsers = async () => {
+    const res = await axios
+      .get("/api/user")
+      .catch((error: { message: any }) => {
+        console.log("catch: ", error.message);
+      });
+
+    if (res && res.data) {
+      setUsers(res.data);
+      console.log("GetUsers->res.data: ", res.data);
+    }
+  };
+
+  useEffect(() => {
+    GetUsers();
+  }, []);
+
+  const addUser = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const resp = await axios.post("/api/user", {
+      Login: user.Login,
+      Email: user.Email,
+      Password: user.Password,
+    });
+
+    if (resp && resp.data) {
+      console.log("AddUser->resp.data: ", resp.data);
+      GetUsers();
+    }
+
+    ResetUser();
+  };
+
+  const UpdateUser = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const resp = await axios.put("/api/user/", {
+      Id: user.Id,
+      Login: user.Login,
+      Email: user.Email,
+      Password: user.Password,
+    });
+
+    if (resp && resp.data) {
+      console.log("UpdateUser->resp.data: ", resp.data);
+      GetUsers();
+    }
+
+    ResetUser();
+  };
+
+  const ResetUser = () => {
+    setUser((prevState) => ({
+      ...prevState,
+      Id: 0,
+      Login: "",
+      Email: "",
+      Password: "",
+    }));
+  };
+
+  const EditUser = async (userId: number) => {
+    const userFound = users.find((user) => user.Id === userId);
+    if (userFound) {
+      setUser(userFound);
+    }
+  };
+
+  const DeleteUser = async (userId: number) => {
+    const resp = await axios
+      .delete("/api/user", {
+        params: { Id: userId },
+      })
+      .catch((error: { message: any }) => {
+        console.log("catch: ", error.message);
+      });
+
+    if (resp && resp.data) {
+      console.log("DeleteUser->resp.data: ", resp.data);
+      GetUsers();
+    }
+  };
+
+  const HandleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setUser((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold mb-4">Users</h1>
+      <form className="mb-4">
+        <input
+          onChange={HandleChange}
+          value={user.Login}
+          type="text"
+          name="Login"
+          placeholder="Login"
+          className="border border-gray-300 rounded-md py-2 px-4 mb-2"
         />
-      </div>
+        <br />
+        <input
+          onChange={HandleChange}
+          value={user.Email}
+          type="email"
+          name="Email"
+          placeholder="Email"
+          className="border border-gray-300 rounded-md py-2 px-4 mb-2"
+        />
+        <br />
+        <input
+          onChange={HandleChange}
+          value={user.Password}
+          type="password"
+          name="Password"
+          placeholder="Password"
+          className="border border-gray-300 rounded-md py-2 px-4 mb-2"
+        />
+        <br />
+        <div className="mt-2">
+          <button
+            onClick={addUser}
+            className="bg-blue-500 text-white py-2 px-4 rounded-md mr-2"
+          >
+            Add User
+          </button>
+          <button
+            onClick={UpdateUser}
+            className="bg-green-500 text-white py-2 px-4 rounded-md"
+          >
+            Update User
+          </button>
+        </div>
+      </form>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <table className="border border-gray-300 rounded-md w-full">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 px-4 py-2">ID</th>
+            <th className="border border-gray-300 px-4 py-2">Login</th>
+            <th className="border border-gray-300 px-4 py-2">Email</th>
+            <th className="border border-gray-300 px-4 py-2">Password</th>
+            <th className="border border-gray-300 px-4 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: User) => (
+            <tr key={user.Id}>
+              <td className="border border-gray-300 px-4 py-2">{user.Id}</td>
+              <td className="border border-gray-300 px-4 py-2">{user.Login}</td>
+              <td className="border border-gray-300 px-4 py-2">{user.Email}</td>
+              <td className="border border-gray-300 px-4 py-2">
+                {user.Password}
+              </td>
+              <td className="border border-gray-300 px-4 py-2">
+                <button
+                  onClick={() => EditUser(user.Id)}
+                  className="bg-yellow-500 text-white py-1 px-2 rounded-md mr-2"
+                >
+                  Editar
+                </button>
+                <button
+                  onClick={() => DeleteUser(user.Id)}
+                  className="bg-red-500 text-white py-1 px-2 rounded-md"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
+
+// import { User } from "@prisma/client";
+// import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+// import axios from "axios";
+
+// export default function Home() {
+//   const [users, setUsers] = useState<User[]>([]);
+//   const [user, setUser] = useState<User>({
+//     Id: 0,
+//     Login: "",
+//     FirstName: "",
+//     LastName: "",
+//     FullName: "",
+//     DisplayName: "",
+//     Email: "",
+//     Password: "",
+//     RegDate: new Date(),
+//     UpdatedDate: new Date(),
+//   });
+
+//   const GetUsers = async () => {
+//     const res = await axios
+//       .get("/api/user")
+//       .catch((error: { message: any }) => {
+//         console.log("catch: ", error.message);
+//       });
+
+//     if (res && res.data) {
+//       setUsers(res.data);
+//       console.log("GetUsers->res.data: ", res.data);
+//     }
+//   };
+
+//   useEffect(() => {
+//     GetUsers();
+//   }, []);
+
+//   const addUser = async (e: SyntheticEvent) => {
+//     e.preventDefault();
+
+//     const resp = await axios.post("/api/user", {
+//       Login: user.Login,
+//       Email: user.Email,
+//       Password: user.Password,
+//     });
+
+//     if (resp && resp.data) {
+//       console.log("AddUser->resp.data: ", resp.data);
+//       GetUsers();
+//     }
+
+//     ResetUser();
+//   };
+
+//   const UpdateUser = async (e: SyntheticEvent) => {
+//     e.preventDefault();
+
+//     const resp = await axios.put("/api/user/", {
+//       Id: user.Id,
+//       Login: user.Login,
+//       Email: user.Email,
+//       Password: user.Password,
+//     });
+
+//     if (resp && resp.data) {
+//       console.log("UpdateUser->resp.data: ", resp.data);
+//       GetUsers();
+//     }
+
+//     ResetUser();
+//   };
+
+//   const ResetUser = () => {
+//     setUser((prevState) => ({
+//       ...prevState,
+//       Id: 0,
+//       Login: "",
+//       Email: "",
+//       Password: "",
+//     }));
+//   };
+
+//   const EditUser = async (userId: number) => {
+//     const userFound = users.find((user) => user.Id === userId);
+//     if (userFound) {
+//       setUser(userFound);
+//     }
+//   };
+
+//   const DeleteUser = async (userId: number) => {
+//     const resp = await axios
+//       .delete("/api/user", {
+//         params: { Id: userId },
+//       })
+//       .catch((error: { message: any }) => {
+//         console.log("catch: ", error.message);
+//       });
+
+//     if (resp && resp.data) {
+//       console.log("DeleteUser->resp.data: ", resp.data);
+//       GetUsers();
+//     }
+//   };
+
+//   const HandleChange = (e: ChangeEvent<HTMLInputElement>) =>
+//     setUser((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
+
+//   return (
+//     <main style={{ marginLeft: "1rem", marginTop: "5px" }}>
+//       <h1>Users</h1>
+//       <form>
+//         <input
+//           onChange={HandleChange}
+//           value={user.Login}
+//           type="text"
+//           name="Login"
+//           placeholder="Login"
+//         />
+//         <br />
+//         <input
+//           onChange={HandleChange}
+//           value={user.Email}
+//           type="email"
+//           name="Email"
+//           placeholder="Email"
+//         />
+//         <br />
+//         <input
+//           onChange={HandleChange}
+//           value={user.Password}
+//           type="password"
+//           name="Password"
+//           placeholder="Password"
+//         />
+//         <br />
+//         <div style={{ marginTop: "5px" }}>
+//           <button onClick={addUser}>Add User</button>
+//           <button onClick={UpdateUser}>Update User</button>
+//         </div>
+//       </form>
+
+//       <table style={{ border: "solid 2px", padding: "5px", marginTop: "12px" }}>
+//         <thead>
+//           <tr>
+//             <th>ID</th>
+//             <th>Login</th>
+//             <th>Email</th>
+//             <th>Password</th>
+//             <th>Actions</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {users.map((user: User) => (
+//             <tr key={user.Id}>
+//               <td>{user.Id}</td>
+//               <td>{user.Login}</td>
+//               <td>{user.Email}</td>
+//               <td>{user.Password}</td>
+//               <td>
+//                 <button onClick={() => EditUser(user.Id)}>Editar</button>
+//                 <button onClick={() => DeleteUser(user.Id)}>Delete</button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </main>
+//   );
+// }
